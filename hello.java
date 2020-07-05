fad28be1dd7fa11b1c2751a9eccbbf4b1683b8a5
Preston Carroll <plutusinc1@yahoo.com>
2020-07-05 12:30:10 -0500

    if else test

--- hello.java
+++ hello.java
@@ -3,6 +3,21 @@ public class hello {
     public static void main(String[] args) {
 
         System.out.println(" hello everybody");
+        System.out.println(1 + 1);
+
+
+         String greeting = "hi";
+         int age = 23;
+
+
+        if (greeting == "hi" && age == 23) {
+            System.out.println(" yes it works");
+        } else {
+            System.out.println(" whoops we had an error");
+        }
+
+
+        
     }
 
 }
