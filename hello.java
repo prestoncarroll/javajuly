713930c9ca69c0b9b9d5c0ab35fbf6bd0f66f7cc
Preston Carroll <plutusinc1@yahoo.com>
2020-07-05 15:51:06 -0500

    args testing complete

--- hello.java
+++ hello.java
@@ -1,23 +1,20 @@
 public class hello {
 
+    // public static void main(String[] args) {
     public static void main(String[] args) {
-
-        System.out.println(" hello everybody");
-        System.out.println(1 + 1);
-
-
-         String greeting = "hi";
-         int age = 23;
-
-
-        if (greeting == "hi" && age == 23) {
-            System.out.println(" yes it works");
-        } else {
-            System.out.println(" whoops we had an error");
-        }
-
-
-        
+        System.out.println("hello00 " + args[0] + " and " + args[1]);
     }
+
+    // System.out.println("-hello everybody");
+    // System.out.println("-" + 1 + 1);
+
+    // String greeting = "hi";
+    // int age = 23;
+
+    // if (greeting == "hi" && age == 23) {
+    // System.out.println("-" + "yes it works");
+    // } else {
+    // System.out.println("-" + "whoops we had an error");
+    // }
 
 }
